cd3fcd8c47fb96330ad680a81806b772a0a633a0
rioyoung3 <rioyoung3@gmail.com>
2017-05-09 16:47:59 +0800

    CV-template

---
+++ shell.sh
@@ -0,0 +1,6 @@
+echo "# Learn-LaTex" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:rioyoung3/Learn-LaTex.git
+git push -u origin master
